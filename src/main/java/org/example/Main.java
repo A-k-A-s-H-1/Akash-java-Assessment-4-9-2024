f83af95bbba46c8b5c07fc6572447c8ca383ebfb
A-k-A-s-H-1 <123860531+A-k-A-s-H-1@users.noreply.github.com>
2024-09-04 17:12:33 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,40 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Shape circle = new Circle(5.0);
-        Shape rectangle = new Rectangle(4.0, 6.0);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Shape Name:"+circle.getName());
-        System.out.println("Area: " + circle.area());
-        System.out.println("Perimeter: " + circle.perimeter());
+        while (true) {
+            System.out.println("Enter the shape you want to create (circle,rectangle) or type 'exit' to quit:");
+            String shapeType = scanner.nextLine().toLowerCase();
 
-        System.out.println("Shape Name:"+rectangle.getName());
-        System.out.println("Area: " + rectangle.area());
-        System.out.println("Perimeter: " + rectangle.perimeter());
+            if (shapeType.equals("exit")) {
+                System.out.println("Exiting the program.");
+                break;
+            }
+
+            switch (shapeType) {
+                case "circle":
+                    System.out.println("Enter the radius of the circle:");
+                    double radius = scanner.nextDouble();
+                    scanner.nextLine(); 
+                    Circle circle = new Circle(radius);
+                    System.out.println("Area of Circle: " + circle.calculateArea());
+                    break;
+                case "rectangle":
+                    System.out.println("Enter the width of the rectangle:");
+                    double width = scanner.nextDouble();
+                    System.out.println("Enter the height of the rectangle:");
+                    double height = scanner.nextDouble();
+                    scanner.nextLine();
+                    Rectangle rectangle = new Rectangle(width, height);
+                    System.out.println("Area of Rectangle: " + rectangle.calculateArea());
+                    break;
+                default:
+                    System.out.println("Invalid shape type. Please try again.");
+            }
+        }
+
+        scanner.close();
     }
-}
+    }
+
